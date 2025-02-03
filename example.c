95cb0278b8c07503963a91478886798ad67e9b74
prithvisnehal <prithvisnehal1006@gmail.com>
2025-02-03 20:50:12 +0000

    Add files via upload

---
+++ example.c
@@ -0,0 +1,13 @@
+// Simple C program to display "Hello World"
+
+// Includes
+#include <stdio.h>
+  
+// Main function
+int main(void) {
+
+    // Prints hello world
+    printf("Hello World!\n");
+  
+    return 0;
+} // end main
